732626941c448f02f6c32c0e14ba89af40dcd502
Tim Wheeler <timothy.wheeler@virginpulse.com>
2020-01-26 00:31:18 -0500

    working on Reverse Integer

--- java/src/main/java/Application.java
+++ java/src/main/java/Application.java
@@ -1,7 +1,9 @@
 public class Application {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] elements = { 1, 2, 3, 1 };
-        solution.findPeakElement(elements);
+//        int[] elements = { 1, 2, 3, 1 };
+//        solution.findPeakElement(elements);
+        System.out.println("Reverse: " + solution.reverse(4321));
+        solution.reverse(4321);
     }
 }
--- java/src/main/java/Solution.java
+++ java/src/main/java/Solution.java
@@ -1,15 +1,31 @@
+//class Solution {
+//    int findPeakElement(int[] nums) {
+//        int index = 0;
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//        for (int i = 1; i < nums.length; i++) {
+//            if (nums[i-1] < nums[i] && nums[i+1] < nums[i]) {
+//                index = i;
+//            }
+//        }
+//        System.out.println("index:" + index);
+//        return index;
+//    }
+//}
+
 class Solution {
-    int findPeakElement(int[] nums) {
-        int index = 0;
-        if (nums == null || nums.length == 0) {
-            return 0;
+    public int reverse(int x) {
+
+        int intAsAbsolute = Math.abs(x);
+        String absoluteAsString = String.valueOf(intAsAbsolute);
+        int length = absoluteAsString.length();
+
+        String reversed = "";
+
+        for (int i = (length - 1); i >= 0; i--) {
+            reversed += absoluteAsString.charAt(i);
         }
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i-1] < nums[i] && nums[i+1] < nums[i]) {
-                index = i;
-            }
-        }
-        System.out.println("index:" + index);
-        return index;
+        return Integer.parseInt(reversed);
     }
 }
